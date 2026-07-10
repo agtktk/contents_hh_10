7304dc3d5bfaf109b949f6fcfa67900370809afd
alltk <97291524+alltk@users.noreply.github.com>
2026-07-10 09:53:15 +0900

    start

---
+++ site.config.js
@@ -0,0 +1,18 @@
+export const siteConfig = {
+  siteName: 'skt정보이용료 희망티켓',
+  siteUrl: 'https://contents-hh-10.pages.dev',
+  siteDescription: 'skt정보이용료 한도 조회 및 이용 안내. SK 통신사 맞춤형 서비스로 24시간 언제나 신속하게 처리 가능합니다.',
+  author: '희망티켓 010-9469-1051',
+  defaultImage: '/images/sample.png',
+  naverVerificationCode: '',
+  googleVerificationCode: '',
+  company: '희망티켓',
+  phone: '01094691051',
+  sms: '01094691051',
+  kakao: 'http://qr.kakao.com/talk/zrNCO4F1ucxqNV7PA9CStjIJt3A-',
+  floatingButtons: {
+    phone: 'tel:01094691051',
+    sms: 'sms:01094691051',
+    kakao: 'http://qr.kakao.com/talk/zrNCO4F1ucxqNV7PA9CStjIJt3A-'
+  }
+};
