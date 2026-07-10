e8e6f9162c709bedd736bafc54970ca384c0860c
alltk <97291524+alltk@users.noreply.github.com>
2026-07-10 11:50:04 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -4,7 +4,7 @@ export const siteConfig = {
   siteDescription: 'skt정보이용료 한도 조회 및 이용 안내. SK 통신사 맞춤형 서비스로 24시간 언제나 신속하게 처리 가능합니다.',
   author: '희망티켓 010-9469-1051',
   defaultImage: '/images/sample.png',
-  naverVerificationCode: '',
+  naverVerificationCode: '934c5e9d57d161b4d2ff85c391106215e97ac86c',
   googleVerificationCode: '',
   company: '희망티켓',
   phone: '01094691051',
